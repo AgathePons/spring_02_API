e36fe255d24c56c3b5d0dac3763bd72e12594011
AgathePons <ponsagathe37@gmail.com>
2022-10-11 15:18:35 +0200

    init Spring API project

---
+++ src/main/java/com/agathepons/apituto/ApiTutoApplication.java
@@ -0,0 +1,13 @@
+package com.agathepons.apituto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agathepons/apituto/ApiTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agathepons.apituto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
